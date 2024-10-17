832ccc280273f6b47d6a73690e508087f075a4ea
bholeram07 <bholeram@gkmit.co>
2024-10-17 20:35:55 +0530

    feat(url:name): added a name to the url path

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from .views import *
 
 urlpatterns = [
-    path('signup/', Signup.as_view()),
-    path('get/', UserProfile.as_view()),
-    path('login/', Login.as_view()),
-    path('update-password/', UpdatePassword.as_view()),
-    path('reset-password/',SendResetPasswordEmail.as_view()),
-    path('reset-password/<user_id>/<token>/', ResetPassword().as_view()),
+    path("signup/", Signup.as_view(), name="signup-user"),
+    path("get-profile/", UserProfile.as_view(), name="user-profile"),
+    path("login/", Login.as_view(), name="login-user"),
+    path("update-password/", UpdatePassword.as_view(), name="update-password"),
+    path("reset-password/send-email/", SendResetPasswordEmail.as_view(),name="reset-password-send-email",),
+    path("reset-password/<user_id>/<token>/",ResetPassword().as_view(), name="reset-password",
+    ),
 ]
