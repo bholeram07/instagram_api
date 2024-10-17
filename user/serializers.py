42c9432f9f112ab475f872acba784a30c89626a6
bholeram07 <bholeram@gkmit.co>
2024-10-17 23:56:36 +0530

    feat(password_validation): added a password validation by regular expression

--- user/serializers.py
+++ user/serializers.py
@@ -4,6 +4,7 @@ from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeErr
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from .utils import *
+import re
 
 
 class SignupSerializer(serializers.ModelSerializer):
@@ -26,19 +27,16 @@ class SignupSerializer(serializers.ModelSerializer):
         password = data.get("password")
         confirm_password = data.get("confirm_password")
         username = data.get("username")
-        if password != confirm_password:
-            raise serializers.ValidationError(
-                "Password does not match with confirm password"
-            )
-        common_password = ["12345678"]
-        if len(password) < 7: #add special symbolize
-            raise serializers.ValidationError(
-                "The length of the password must be more than 7"
-            )
-        if password in common_password:
-            raise serializers.ValidationError(
-                "The Password is too common please choose different password"
-            )
+        if not re.search(r"[A-Z]", password): 
+            raise serializers.ValidationError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[a-z]", password):  
+            raise serializers.ValidationError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[0-9]", password): 
+            raise serializers.ValidationError("Password must contain at least one digit.")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):  
+           raise serializers.ValidationError("Password must contain at least one special character.")
+        if re.search(r"\s", password):  
+           raise serializers.ValidationError("Password must not contain any spaces.")
         if password == username:
             raise serializers.ValidationError("Password not be username")
         return super().validate(data)
@@ -86,7 +84,7 @@ class SendResetPasswordEmailSerializer(serializers.Serializer):
 
     class Meta:
         fields = ("email",)
-
+        
     def validate(self, attrs):
         email = attrs.get("email")
         if not User.objects.filter(email=email).exists():
@@ -95,23 +93,18 @@ class SendResetPasswordEmailSerializer(serializers.Serializer):
 
 
 class ResetPasswordSerializer(serializers.Serializer):
-    password = serializers.CharField(
-        max_length=20, style={"input_type : password"}, write_only=True
-    )
-    confirm_password = serializers.CharField(
-        max_length=20, style={"input_type : password"}, write_only=True
-    )
-
+    new_password = serializers.CharField(max_length=20, style={"input_type : password"}, write_only=True)
+    confirm_password = serializers.CharField(max_length=20, style={"input_type : password"}, write_only=True)
     class Meta:
         fields = ("password", "confirm_password")
 
     def validate(self, attrs):
-        password = attrs.get("password")
+        new_password = attrs.get("password")
         confirm_password = attrs.get("confirm_password")
         user_id = self.context.get("user_id")
         token = self.context.get("token")
 
-        if password != confirm_password:
+        if new_password != confirm_password:
             raise serializers.ValidationError(
                 "password does not match with the confirm password"
             )
@@ -120,6 +113,6 @@ class ResetPasswordSerializer(serializers.Serializer):
         user = User.objects.get(id=id)
         if not PasswordResetTokenGenerator().check_token(user, token):
             raise serializers.ValidationError("Token is not matched or expired")
-        user.set_password(password)
+        user.set_password(new_password)
         user.save()
         return attrs
