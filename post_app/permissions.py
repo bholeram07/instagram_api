4ca065f3c949799fffaef47ef2cf4711563fea04
bholeram07 <bholeramgkmit.co>
2024-10-21 12:33:12 +0530

    feat(permisisions.py): added a custom permission

---
+++ post_app/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrCommentAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.post.user == request.user:
+            return True  
+        return obj.user == request.user  # User can only delete their own comment
