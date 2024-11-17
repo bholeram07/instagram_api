ac6f036c15bd5c95f4e0e6320a578df54d9accab
bholeram07 <bholeram@gkmit.co>
2024-11-17 16:12:33 +0530

    "fix(middleware-bug)"

--- post_app/middleware.py
+++ post_app/middleware.py
@@ -16,16 +16,13 @@ class UUIDValidationMiddleware:
     def check_uuid_in_url(self, request):
         try:
             resolved = resolve(request.path_info)
-            print(f"Resolved view: {resolved}")
             url_params = resolved.kwargs 
-            print(f"URL Params: {url_params}")
+           
         except Exception as e:
-            print(f"Exception resolving path: {e}")
             return None 
 
         for param, value in url_params.items():
             if not self.is_valid_uuid(value):
-                print(f"Invalid UUID: {value} for param {param}")
                 return JsonResponse(
                     {"detail": f"The provided {param} UUID is not in a valid format."},
                     status=400
@@ -33,15 +30,12 @@ class UUIDValidationMiddleware:
         return None
 
     def is_valid_uuid(self, value):
-        print(f"Validating UUID: {value}")
         if isinstance(value, str):
             try:
                 uuid_obj = uuid.UUID(value)
                 is_valid = str(uuid_obj) == value
-                print(f"Validation result for {value}: {is_valid}")
                 return is_valid
             except ValueError:
-                print(f"Validation failed for {value}")
+                
                 return False
-        print(f"Value {value} is not a string")
         return False
